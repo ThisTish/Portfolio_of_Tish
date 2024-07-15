f85b617acbffce4c90caed255fe567810aadecd6
Tish <tishthegoat@gmail.com>
2024-07-15 16:34:00 -0600

    initial setup

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-5xl font-bold text-lime-900'>This is only the beginning</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
